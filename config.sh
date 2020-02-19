9ebd96b22a1802a517d015a806fcbfba5053b832
Ubuntu <ubuntu@ip-172-31-34-207.us-east-2.compute.internal>
2020-02-19 21:10:05 +0000

    Add a script for auto build enviroment for docker H-container

---
+++ config.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Author: Tong Xing. 
+# Stevens Institute of technology 2020 
+set -x
+set -e
+
+sudo apt-get update
+sudo apt-get install docker.io -y
+sudo docker run hello-world
+sudo bash -c 'echo -e "{\n\t\"experimental\": true\n}" >> /etc/docker/daemon.json'
+sudo service docker restart
+sudo apt-get update && sudo apt-get install -y protobuf-c-compiler libprotobuf-c0-dev protobuf-compiler libprotobuf-dev:amd64 gcc build-essential bsdmainutils python git-core asciidoc make htop git curl supervisor cgroup-lite libapparmor-dev libseccomp-dev libprotobuf-dev libprotobuf-c0-dev protobuf-c-compiler protobuf-compiler python-protobuf libnl-3-dev libcap-dev libaio-dev apparmor libnet1-dev 1>/dev/null
+
+git clone https://github.com/systems-nuts/criu.git
+cd criu && git checkout heterogeneous-simplified
+cpucore=sudo cat /proc/cpuinfo |grep "processor" |wc -l
+sudo make -j$cpucore
+sudo make install
