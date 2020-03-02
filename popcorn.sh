9fad290eed9c816788de8384a4d2574a64087577
123toorc <123toorc@gmail.com>
2020-03-02 22:54:56 +0000

    enable port mapping, add -p flag, successfully test redis migration

--- popcorn.sh
+++ popcorn.sh
@@ -2,19 +2,21 @@
 # Author: Tong Xing
 # Stevens Institute of Technology 2020
 set -e
-#set -x
+set -x
 
 DIR=$1
 TARGET_MACHINE=$2
-
+PORT_FLAG=$3
+PORT_BIND=$4
 help()
 {
     cat <<- EOF
 Desc: Popcorn is for migrate container cross ISAs
-Usage: ./popcorn.sh <Container DIR> <TARGET Machine>
+Usage: ./popcorn.sh <Container DIR> <TARGET Machine> [-p] [PORT:PORT]
     - Container DIR is the path of directory store the Dockerfile to build a docker image
     - TARGET Machine is the target machine user@ip. example: popcorn@10.4.4.111
-Example: ./popcorn.sh ./helloworld ubuntu@172.31.23.242
+    - PORT FLAG is -p to indicate port is mapping by host:guest 
+Example: ./popcorn.sh ./helloworld ubuntu@172.31.23.242 [-p] [1111:1111]
 Author: Tong Xing
 Stevens Institute of Technology 2020
 EOF
@@ -28,13 +30,18 @@ while [ -n "$1" ];do
 		*) break;;
 esac
 done
-if [ $# != 2 ]
+if [ $# != 2 -a $#!=4]
 then 
     help
 fi
 DIR=$(bash -c "echo $(cd $DIR ; pwd)")
 WDIR=$(cat $DIR/Dockerfile | grep WORKDIR | awk '{print $2}' | sed -n '1p')
-echo check cgroup
+if [ -d $WDIR ];
+then
+	sudo rm -r $WDIR
+else
+	echo fuck
+fi
 if [ -f "./scripts/check.sh" ]
 then
 	echo local cgroup..........
@@ -47,12 +54,12 @@ fi
 
 BIN=$(ls $DIR | grep aarch64)
 EXE=$(echo ${BIN%_*})
-echo EXE
+echo $PORT_FLAG $PORT_BIND
 
 
 if [ -f "./scripts/builder.sh" ]
 then
-	CID=$(sudo bash -c "./scripts/builder.sh $DIR")
+	CID=$(sudo bash -c "./scripts/builder.sh $DIR $PORT_FLAG $PORT_BIND")
 else
         echo  "builder.sh can't find..." || exit 1
 fi
@@ -72,7 +79,7 @@ CHECKPOINT="/tmp/check_hcontainer"
 echo start restore
 if [ -f "./scripts/restore.sh" ]
 then
-        bash -c "cd scripts; ./restore.sh $DIR $TARGET_MACHINE $CHECKPOINT"
+        bash -c "cd scripts; ./restore.sh $DIR $TARGET_MACHINE $CHECKPOINT $PORT_FLAG $PORT_BIND"
 else
         echo  "restore.sh can't find..." || exit 1
 fi
--- scripts/builder.sh
+++ scripts/builder.sh
@@ -4,15 +4,17 @@
 # Author Tong Xing 
 # Stevens Institute of Technology
 # This script will build a container  
-#set -x
+set -x
 DIR=$1
-
+PORT_FLAG=$2
+PORT_BIND=$3
 help()
 {
     cat <<- EOF
 Desc: Build is a helper function to help build a H-container
-Usage: ./builder.sh <Container DIR> <Work DIR>
+Usage: ./builder.sh <Container DIR> [Port_Flage] [PORT:PORT]
     - Container DIR is the directory store the Dockerfile to build a docker image
+    - with -p as flag, indicate a port mapping with host and container 
 Example: ./builder.sh ./helloworld popcorn-hello
 Author: Tong Xing
 Stevens Institute of Technology 2020
@@ -27,11 +29,12 @@ while [ -n "$1" ];do
                 *) break;;
 esac
 done
-if [ $# != 1 ]
+if [ $# != 1 -a $# != 3 ]
 then
     help
 fi
-
+HOST_PORT=${PORT_BIND%%:*}
+CONTAINER_PORT=${PORT_BIND##*:}
 BIN=$(ls $DIR | grep aarch64)
 EXE=$(echo ${BIN%_*})
 WDIR=$(cat $DIR/Dockerfile | grep WORKDIR | awk '{print $2}' | sed -n '1p')
@@ -46,6 +49,13 @@ fi
 cd $DIR  && cp $EXE'_'$arch $EXE 	&& sudo docker build -t hcontainer . 1>/dev/null 2>&1
 CID=$(sudo docker container create hcontainer)
 sudo sed -i 's/"CapAdd":null/"CapAdd":["all"]/' /var/lib/docker/containers/$CID/hostconfig.json
+if [ "$2" = "-p" ]
+then
+	sudo sed -i "s/\"PortBindings\":{}/\"PortBindings\":{\"$CONTAINER_PORT\/tcp\":[{\"HostIp\":\"\",\"HostPort\":\"$HOST_PORT\"}]}/" /var/lib/docker/containers/$CID/hostconfig.json
+	sudo sed -i "s/\"AttachStderr\":false,/\"AttachStderr\":false,\"ExposedPorts\":{\"$CONTAINER_PORT\/tcp\":{}},/" /var/lib/docker/containers/$CID/config.v2.json
+	sudo sed -i "s/\"AttachStderr\":true,/\"AttachStderr\":false,\"ExposedPorts\":{\"$CONTAINER_PORT\/tcp\":{}},/" /var/lib/docker/containers/$CID/config.v2.json
+	sudo sed -i "s/\"Ports\":{}/\"Ports\":{\"$CONTAINER_PORT\/tcp\":[{\"HostIp\":\"0.0.0.0\",\"HostPort\":\"$HOST_PORT\"}]}/" /var/lib/docker/containers/$CID/config.v2.json
+fi
 sudo service docker restart 1>/dev/null 2>&1 
 echo $CID
 
